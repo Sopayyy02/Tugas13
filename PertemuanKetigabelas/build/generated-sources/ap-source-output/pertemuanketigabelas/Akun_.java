536138d09cc617f57e17b277b28c9c5ede5a8033
Shofie Fadliya Rahma <rahmashofiesk@gmail.com>
2025-11-15 19:34:26 +0700

    Add files via upload

---
+++ PertemuanKetigabelas/build/generated-sources/ap-source-output/pertemuanketigabelas/Akun_.java
@@ -0,0 +1,16 @@
+package pertemuanketigabelas;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-11-15T01:13:23", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Akun.class)
+public class Akun_ { 
+
+    public static volatile SingularAttribute<Akun, String> password;
+    public static volatile SingularAttribute<Akun, Integer> id;
+    public static volatile SingularAttribute<Akun, String> email;
+    public static volatile SingularAttribute<Akun, String> username;
+
+}
---
+++ PertemuanKetigabelas/src/pertemuanketigabelas/Akun.java
@@ -0,0 +1,119 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuanketigabelas;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author VIVOBOOK 14
+ */
+@Entity
+@Table(name = "akun")
+@NamedQueries({
+    @NamedQuery(name = "Akun.findAll", query = "SELECT a FROM Akun a"),
+    @NamedQuery(name = "Akun.findById", query = "SELECT a FROM Akun a WHERE a.id = :id"),
+    @NamedQuery(name = "Akun.findByEmail", query = "SELECT a FROM Akun a WHERE a.email = :email"),
+    @NamedQuery(name = "Akun.findByUsername", query = "SELECT a FROM Akun a WHERE a.username = :username"),
+    @NamedQuery(name = "Akun.findByPassword", query = "SELECT a FROM Akun a WHERE a.password = :password")})
+public class Akun implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "email")
+    private String email;
+    @Basic(optional = false)
+    @Column(name = "username")
+    private String username;
+    @Basic(optional = false)
+    @Column(name = "password")
+    private String password;
+
+    public Akun() {
+    }
+
+    public Akun(Integer id) {
+        this.id = id;
+    }
+
+    public Akun(Integer id, String email, String username, String password) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Akun)) {
+            return false;
+        }
+        Akun other = (Akun) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "pertemuanketigabelas.Akun[ id=" + id + " ]";
+    }
+    
+}
